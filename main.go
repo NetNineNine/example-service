12800639628149dc557edefdd4d45f2bd36b0ea1
Wynand Pieters <wynandp@netninenine.co.za>
2024-10-19 14:57:54 +0200

    feat: add logging and build info
    use slog for logging instead of println and print build version if set

--- main.go
+++ main.go
@@ -2,19 +2,36 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"time"
+)
+
+var (
+	version   string
+	commitSHA string
+	buildDate string
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Hello, World!")
+	fmt.Fprintln(w, "Hello, World!")
 }
 
 func main() {
-    http.HandleFunc("/", helloHandler)
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
 
-    fmt.Println("Starting server on :8080")
-    if err := http.ListenAndServe(":8080", nil); err != nil {
-        log.Fatalf("Could not start server: %s\n", err)
-    }
+	http.HandleFunc("/", helloHandler)
+
+	logger.Info("Starting server on :8080")
+	if len(version) == 0 {
+		version = "local"
+		commitSHA = "none"
+		buildDate = time.Now().Format(time.RFC3339)
+	}
+	logger.Info(fmt.Sprintf("Version: %s, commitSHA: %s, buildDate: %s", version, commitSHA, buildDate))
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		slog.Error("Could not start server", "error", err)
+	}
 }
